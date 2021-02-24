154b5529920e473aeab48b79c0493db7ef1778bb
n2qzshce <WJgBEMF4@outlook.com>
2021-02-23 17:26:04 -0700

    Added validation system.

--- ham/util/validator.py
+++ ham/util/validator.py
@@ -1,6 +1,10 @@
 import logging
 
+from ham.dmr.dmr_contact import DmrContact
+from ham.dmr.dmr_id import DmrId
+from ham.dmr.dmr_user import DmrUser
 from ham.radio.radio_channel import RadioChannel
+from ham.radio.radio_zone import RadioZone
 from ham.util import radio_types
 from ham.util.data_column import DataColumn
 from ham.util.validation_error import ValidationError
@@ -9,13 +13,33 @@ from ham.util.validation_error import ValidationError
 class Validator:
 	def __init__(self):
 		self._radio_channel_template = RadioChannel.create_empty()
+		self._digital_contact_template = DmrContact.create_empty()
+		self._dmr_id_template = DmrId.create_empty()
+		self._zone_template = RadioZone.create_empty()
+		self._dmr_user_template = DmrUser.create_empty()
 		return
 
-	def validate_radio_channel(self, cols, line, file_name):
-		needed_cols_dict_gen = dict(self._radio_channel_template.__dict__)
-		return self._validate_generic(cols, line, file_name, needed_cols_dict_gen)
+	def validate_dmr_user(self, cols, line_num, file_name):
+		needed_cols_dict_gen = dict(self._dmr_user_template.__dict__)
+		return self._validate_generic(cols, line_num, file_name, needed_cols_dict_gen)
 
-	def _validate_generic(self, cols, line, file_name, needed_cols_dict_gen):
+	def validate_radio_zone(self, cols, line_num, file_name):
+		needed_cols_dict_gen = dict(self._zone_template.__dict__)
+		return self._validate_generic(cols, line_num, file_name, needed_cols_dict_gen)
+
+	def validate_dmr_id(self, cols, line_num, file_name):
+		needed_cols_dict_gen = dict(self._dmr_id_template.__dict__)
+		return self._validate_generic(cols, line_num, file_name, needed_cols_dict_gen)
+
+	def validate_digital_contact(self, cols, line_num, file_name):
+		needed_cols_dict_gen = dict(self._digital_contact_template.__dict__)
+		return self._validate_generic(cols, line_num, file_name, needed_cols_dict_gen)
+
+	def validate_radio_channel(self, cols, line_num, file_name):
+		needed_cols_dict_gen = dict(self._radio_channel_template.__dict__)
+		return self._validate_generic(cols, line_num, file_name, needed_cols_dict_gen)
+
+	def _validate_generic(self, cols, line_num, file_name, needed_cols_dict_gen):
 		errors = []
 		needed_cols = dict()
 
@@ -27,7 +51,7 @@ class Validator:
 
 		for key in cols.keys():
 			if key not in needed_cols.keys():
-				err = ValidationError(f"`{key}` missing from entry.", line, file_name)
+				err = ValidationError(f"`{key}` missing from entry.", line_num, file_name)
 				errors.append(err)
 				continue
 			data_column = needed_cols[key]
@@ -37,11 +61,11 @@ class Validator:
 
 			try:
 				data_column.shape(cols[key])
-			except ValueError as e:
+			except ValueError:
 				shape_name = str(data_column.shape).replace("<class '", '').replace("'>", '')
 				err = ValidationError(
 										f"Error parsing `{cols[key]}` in column `{key}` as `{shape_name}`",
-										line,
+										line_num,
 										file_name
 									)
 				logging.debug(err.message)
